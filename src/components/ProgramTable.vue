949f437ae787e0764ddde1a14d3021dc2e773a8d
Lijuan Cheng <lijuan.cheng@greaterthan.eu>
2022-12-07 14:28:24 +0100

    got first data

---
+++ src/components/ProgramTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <div>Visar:</div>
+      <div v-for="program in programs" :key="program.program.id">
+          <div>Tiden:{{}}</div>
+          <div>Icon:{{}}</div>
+          <div>Title:{{program.title}}
+              <div class="show-more-info">Visa mer+<span>beskrivning:{{}}</span></div>
+          </div>
+      </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgramTable',
+ props:['programs']
+};
+</script>
+<style lang="scss" scoped>
+@import '../assets/scss/main.scss';
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+
+#home {
+  background: #ffffff;
+  height: 100vh;
+  padding: 20px;
+  .title {
+    color: #1c1b1b;
+    font-family: Poppins;
+    font-size: 30px;
+  }
+  .show-more-info{
+      cursor: pointer;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="home">
-  
+    <h2 class="title">Sverige Radio P3:s tablå</h2>
+    <ProgramTable :programs='apiData.schedule'/>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-   
+    ProgramTable:() => import('@/components/ProgramTable.vue')
   },
+  data:() =>({ 
+    apiData: {}
+
+  }),
+  async created() {
+    const res = await axios.get("https://api.sr.se/api/v2/scheduledepisodes?channelid=164&format=json");
+    this.apiData = res.data;
+  }
 };
+
 </script>
-<style  lang ="scss" scoped>
-@import "../assets/scss/main.scss";
+<style lang="scss" scoped>
+@import '../assets/scss/main.scss';
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
 
 #home {
-
-  
- 
+  background: #ffffff;
+  height: 100vh;
+  padding: 20px;
+  .title {
+    color: #1c1b1b;
+    font-family: Poppins;
+    font-size: 30px;
+  }
 }
 </style>
